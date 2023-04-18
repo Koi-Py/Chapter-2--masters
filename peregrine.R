004bc59246f3595c719af8c049dc0ba90afbf871
Koi Pillay <128590580+Koi-Py@users.noreply.github.com>
2023-04-18 15:21:18 +0200

    Add files via upload

--- peregrine.R
+++ peregrine.R
@@ -1,7 +1,6 @@
 #peregrine Arjun 
 
 library("MASS")
-library("fitdistrplus")
 library("dplyr")
 
 setwd("C:/Users/Koi/OneDrive - University of Cape Town/Documents/Masters/Chapter 2/getting started")
@@ -29,7 +28,7 @@ Data <- data.frame(
 
 # Set probability and standard deviation of grouse Moors 
 gprob <- 0.33
-gsd <- 0.05 
+gsd <- 0.05
 
 # Calculate sample size
 gsize <- (gprob*(1-gprob)) / (gsd^2)
@@ -59,11 +58,10 @@ hist(diffnon)
 
 # Set probability and standard deviation of nongrouse Moors 
 prob <- 0.67
-sd <- 0.04 
 
 
 # Generate random sample from binomial distribution
-sample <- rbinom(n = 1000, size = 947, prob = prob)
+sample <- rbinom(n = 1000, size = 22, prob = prob)
 hist(sample, breaks = 20, main = "Binomial Distribution")
 
 Data <- data.frame(
@@ -72,10 +70,9 @@ Data <- data.frame(
 
 # Set probability and standard deviation of grouse Moors 
 gprob <- 0.33
-gsd <- 0.05 
 
 # Generate random sample from binomial distribution
-gsample <- rbinom(n = 1000, size = 947, prob = prob)
+gsample <- rbinom(n = 1000, size = 22, prob = prob)
 hist(sample, breaks = 20, main = "Binomial Distribution")
 
 Data <- data.frame(
@@ -93,16 +90,65 @@ summary(diffnon)
 length(diffnon)
 hist(diffnon)
 
+####
+difference <- rbinom(n = 17, size = 22, prob = 0.34) #estimating for the 17 years
+hist(difference)
+difference #no. persecuted nests for each year
+
+# Calculate sum of the difference dataframe
+sum_difference <- sum(difference)
+
+# Print the result
+cat("Sum of difference: ", sum_difference, "\n")
+####
+
+
+results <- list()
+
+# Set the number of iterations to 100
+n_iterations <- 100
+
+# Loop over the number of iterations
+for (i in 1:n_iterations) {
+  # Generate a random sample using rbinom()
+  difference <- rbinom(n = 17, size = 22, prob = 0.34)
+  
+  # Append the results to the list
+  results[[i]] <- difference
+}
+
+# Print the first 4 elements of the list
+head(results, n = 4)
+
+
+# Calculate mean of the difference dataframe
+mean_difference <- mean(difference)
+
+# Calculate standard error of the mean
+se_difference <- sqrt(var(difference) / length(difference))
+
+# Calculate the 95% confidence interval
+lower_ci <- mean_difference - 1.96 * se_difference
+upper_ci <- mean_difference + 1.96 * se_difference
+
+# Print the results
+cat("Mean of difference: ", mean_difference, "\n")
+cat("95% CI of difference: [", lower_ci, ", ", upper_ci, "]\n")
+
+
 ##load in data of population during years
 peregrine <- read.csv("Peregrine Arjun-grouse moor nests.csv")
 
 meanterri <- mean(peregrine$number.of.occupied.territories)
 sd <- sd(peregrine$number.of.occupied.territories)
 
-no.persecmean <- (diffnon/100) * meanterri
-no.persecsd <- (diffnon/100) * sd
+no.persecmean <- (difference/100) * 22.07
+no.persecsd <- (difference/100) * sd
 
 persecuted <- data.frame(no.persecmean, no.persecsd)
 mean(persecuted$no.persecmean)
 sd(persecuted$no.persecsd)
+
+mean(no.persecmean)
+
 
